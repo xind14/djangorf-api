5ca0a0eeae6b023aa7e92e91f9deb59b00c33f62
Xin Deng <xind1470@gmail.com>
2024-02-20 11:27:53 -0800

    changed database to postgres in settings

--- snacks_api_project/settings.py
+++ snacks_api_project/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'snacks_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
+
 
 
 # Password validation
--- snacks_api_project/urls.py
+++ snacks_api_project/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/snacks/", include("snacks.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+
 ]
